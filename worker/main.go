5122ded3d33b0c40ef66afe9036d0a575b016c42
kstasi <nastasiakondaurova@gmail.com>
2020-07-21 13:59:19 +0300

    worker draft

--- worker/main.go
+++ worker/main.go
@@ -1,2 +1,24 @@
 package worker
 
+import "fmt"
+
+// Worker - struct represented worker info
+type worker struct {
+	ID          uint64
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Email       string
+	Possition   string
+	Company     string
+	Salary      float32
+}
+
+func NewWorker(id uint64, FirstName, LastName, PhoneNumber, Email, Possition, Company string, Salary float32) *Worker {
+	return &worker{id, FirstName, LastName, PhoneNumber, Email, Possition, Company, Salary}
+}
+
+func main() {
+	v := NewWorker(0, "Kate", "Ostin", "380976482598", "kateost@gmail.com", "Manager", "Teamdream", 1000.)
+	fmt.Println(v)
+}
