1a8bbfe81d3adc782bfeffba1bbfd93f78b2f742
kstasi <nastasiakondaurova@gmail.com>
2020-07-30 13:39:18 +0300

    main updated

---
+++ workplaceExample/go.mod
@@ -0,0 +1,5 @@
+module github.com/KStasi/letsgo/workplaceExample
+
+go 1.14
+
+require github.com/KStasi/letsgo/workplace v0.0.0-20200727131149-a8fdaee72e7d
---
+++ workplaceExample/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/KStasi/letsgo/workplace"
+)
+
+func main() {
+
+	worker := workplace.Worker{
+		HumanInfo: workplace.Human{
+			ID: 1023232,
+			FirstName: "Tom",
+			LastName: "Ostin",
+			Sex: workplace.Male,
+			Height: 180,
+			Weight: 70,
+			Age: 20
+		},
+		Possition: "Notary",
+		Company: "Teamdream",
+		Salary: 2000.,
+		PhoneNumber: "380974320191",
+		Email: "roewa@gmail.com",
+	}
+	lawyer := workplace.Lawyer{
+		Workers:          worker,
+		Type:             workplace.Social,
+		LegislationArrea: "Agro",
+	}
+
+	fmt.Println(lawyer)
+	fmt.Printf("%+v\n", lawyer)
+}
