fd052f388b92310818febe0018593245cece1a13
kstasi <nastasiakondaurova@gmail.com>
2020-08-05 09:42:00 +0300

    intrerface implemented

--- workplace/worker.go
+++ workplace/worker.go
@@ -39,6 +39,11 @@ const (
 	Creative
 )
 
+// IWorker - interface to indroduce own profession, position and name
+type IWorker interface {
+	Introduce() (string, string, string)
+}
+
 // Human - struct represented person info
 type Human struct {
 	ID        uint64 `json:"id"`
@@ -52,51 +57,51 @@ type Human struct {
 
 // Worker - struct represented worker info
 type Worker struct {
-	HumanInfo   Human   `json:"human"`
 	Possition   string  `json:"possition"`
 	Company     string  `json:"company"`
 	Salary      float32 `json:"salary"`
 	PhoneNumber string  `json:"phone"`
 	Email       string  `json:"email"`
+	Human
 }
 
 // Doctor - struct represented occupation info
 type Doctor struct {
-	Workers      Worker         `json:"workers"`
 	Type         ProfessionType `json:"type"`
 	Department   string         `json:"department"`
 	Education    string         `json:"education"`
 	WorkingHours string         `json:"working_hours"`
+	Worker
 }
 
 // Developer - struct represented occupation info
 type Developer struct {
-	Workers  Worker         `json:"workers"`
 	Type     ProfessionType `json:"type"`
 	WorkType WorkType       `json:"work_type"`
+	Worker
 }
 
 // Lawyer - struct represented occupation info
 type Lawyer struct {
-	Workers          Worker         `json:"workers"`
 	Type             ProfessionType `json:"type"`
 	LegislationArrea string         `json:"legislation_arrea"`
+	Worker
 }
 
 // Sportsman - struct represented occupation info
 type Sportsman struct {
-	Workers Worker         `json:"workers"`
 	Type    ProfessionType `json:"type"`
 	Rewards []string       `json:"rewards"`
 	Rank    string         `json:"rank"`
+	Worker
 }
 
 // Nurse - struct represented occupation info
 type Nurse struct {
-	Workers        Worker         `json:"workers"`
 	Type           ProfessionType `json:"type"`
 	Experience     int32          `json:"experience"`
 	HasOwnChildren bool           `json:"has_children"`
+	Worker
 }
 
 // CelebrateBirthday - method to make person older
@@ -108,4 +113,29 @@ func (receiver *Human) CelebrateBirthday() {
 func (receiver *Human) UpdateShape(newHeight int, newWeight int) {
 	receiver.Height = newHeight
 	receiver.Weight = newWeight
+}
+
+// Introduce - interface IWorker implemented for Doctor
+func (d Doctor) Introduce() (string, string, string) {
+	return "Doctor", d.Possition, d.FirstName
+}
+
+// Introduce - interface IWorker implemented for Developer
+func (d Developer) Introduce() (string, string, string) {
+	return "Developer", d.Possition, d.FirstName
+}
+
+// Introduce - interface IWorker implemented for Lawyer
+func (l Lawyer) Introduce() (string, string, string) {
+	return "Lawyer", l.Possition, l.FirstName
+}
+
+// Introduce - interface IWorker implemented for Sportsman
+func (s Sportsman) Introduce() (string, string, string) {
+	return "Sportsman", s.Possition, s.FirstName
+}
+
+// Introduce - interface IWorker implemented for Nurse
+func (n Nurse) Introduce() (string, string, string) {
+	return "Nurse", n.Possition, n.FirstName
 }
